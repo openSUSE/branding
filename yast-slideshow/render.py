8e5d1d632b222b4818a36fbd91939a62f8894163
jimmacfx <jimmacfx@95aa0454-cf36-0410-a201-bdd6ffda1122>
2007-08-06 11:39:00 +0000

    slideshow for the installer
    git-svn-id: https://forgesvn1.novell.com/svn/opensuse-art/trunk@13 95aa0454-cf36-0410-a201-bdd6ffda1122

---
+++ yast-slideshow/render.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import os
+import re
+
+res = '150x150'
+files = os.listdir('./')
+#make dirs
+if not os.path.exists('pic'):
+	os.mkdir('pic')
+	print x, "created"
+for file in files:
+	if re.search("svg$",file):
+		png = re.sub(r"([^.]*)\.svg$", r"\1.png", file)
+		print png
+		size = re.sub(r"([^x]*)x.*", r"\1", res)
+		cmd = "/opt/artlibre/bin/inkscape -w %s -h %s -e pic/%s %s" % (size,size,png,file)
+		print "rendering %s at %s" % (file,res)
+		os.system(cmd)
