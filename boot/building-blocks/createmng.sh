a2ef2d8bcca81bee40620694b4f78661a1b65557
jimmacfx <jimmacfx@95aa0454-cf36-0410-a201-bdd6ffda1122>
2007-08-06 09:12:57 +0000

    bootsplash
    git-svn-id: https://forgesvn1.novell.com/svn/opensuse-art/trunk@2 95aa0454-cf36-0410-a201-bdd6ffda1122

---
+++ boot/building-blocks/createmng.sh
@@ -0,0 +1,3 @@
+gm convert -comment "id logo deltabg stop" logo.png logo.png logo.mng
+gm convert -comment "id logov deltabg stop" logov.png logov.png logov.mng
+gm convert -comment "id infotext deltabg stop" infotext.png infotext.png infotext.mng
