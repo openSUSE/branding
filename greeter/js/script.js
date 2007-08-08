5d48dc77815bab08e9612e64384f4436ce500bd4
jimmacfx <jimmacfx@95aa0454-cf36-0410-a201-bdd6ffda1122>
2007-08-08 15:12:59 +0000

    cleaner layout of the greeter
    git-svn-id: https://forgesvn1.novell.com/svn/opensuse-art/trunk@30 95aa0454-cf36-0410-a201-bdd6ffda1122

---
+++ greeter/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$("#header img").fadeIn(2000);
+	setTimeout("$(\"#button\").fadeIn(1000)", 2000);
+	$("#button").click(function() {
+		console.log("clicked");
+		self.close();
+	});
+});
+
+
+
