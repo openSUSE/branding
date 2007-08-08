6c21bac9e4836bed9246c3bff090ff170ba554fc
jimmacfx <jimmacfx@95aa0454-cf36-0410-a201-bdd6ffda1122>
2007-08-08 15:30:24 +0000

    nuke the button fade. buggy in konqueror
    git-svn-id: https://forgesvn1.novell.com/svn/opensuse-art/trunk@33 95aa0454-cf36-0410-a201-bdd6ffda1122

--- greeter/js/script.js
+++ greeter/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	$("#header img").fadeIn(2000);
-	setTimeout("$(\"#button\").fadeIn(1000)", 2000);
+	//setTimeout("$(\"#button\").fadeIn(1000)", 2000);
 	$("#button").click(function() {
 		console.log("clicked");
 		self.close();
