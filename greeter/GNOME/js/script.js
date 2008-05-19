f910ff50608c0c81fd3428c8b99ab0d49b6e1c1e
jimmacfx <jimmacfx@95aa0454-cf36-0410-a201-bdd6ffda1122>
2008-05-19 13:38:45 +0000

    GNOME version (different graphics) of the SUSE greeter.
    git-svn-id: https://forgesvn1.novell.com/svn/opensuse-art/trunk@182 95aa0454-cf36-0410-a201-bdd6ffda1122

---
+++ greeter/GNOME/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  $('#header img').css('top','-30px').animate({top: 5},500);
+  $("#opensuse").css('opacity',0).delayedShow(300,800);
+  $("#support").css('opacity',0).delayedShow(800,800);
+  $("#build").css('opacity',0).delayedShow(1200,800);
+  $("#fun").css('opacity',0).delayedShow(1800,800);
+	//$("#header img").css('opacity',0).delayedShow(1800,2000);
+	$("#button").css('opacity',0).delayedShow(2000,2000);
+});
+
+
+$.fn.delayedShow = function (delaytime,animtime) {
+
+  return $(this).oneTime(delaytime,function () {
+    $(this).animate({'opacity':1},animtime);
+  });
+}
