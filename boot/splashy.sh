d3e0dc17426487e729a5d757693d107678386a37
jimmacfx <jimmacfx@95aa0454-cf36-0410-a201-bdd6ffda1122>
2008-05-19 11:51:31 +0000

    Use a more common resolution for splashy.
    git-svn-id: https://forgesvn1.novell.com/svn/opensuse-art/trunk@180 95aa0454-cf36-0410-a201-bdd6ffda1122

--- boot/splashy.sh
+++ boot/splashy.sh
@@ -7,6 +7,6 @@ splashy="./splashy/background.png"
 splashyg="./splashy/backgroundg.png"
 
 inkscape -w 180 -e $logopng $logosvg
-gm convert -geometry 800x600\! $background $temp
+gm convert -geometry 1024x768\! $background $temp
 gm composite $logopng -compose Over -gravity center $temp $splashy
 gm composite $logopng -compose Over -colorspace gray -gravity center $temp $splashyg
